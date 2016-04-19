358b2a0b16ff791a13dcb5aaaa28d4f6faad4ac5
Ajla Alic <a.ylla@live.com>
2016-04-19 19:05:13 +0200

    Projekat v1.0

---
+++ godisnji-odmori/src/main/java/tim12/si/app/godisnji_odmori/App.java
@@ -0,0 +1,13 @@
+package tim12.si.app.godisnji_odmori;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
