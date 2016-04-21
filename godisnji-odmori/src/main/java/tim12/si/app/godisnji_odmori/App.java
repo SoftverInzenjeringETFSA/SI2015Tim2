1abf015ffb34ae79990a80cf294afad10ce33f1e
kerimbalic <balickerim@gmail.com>
2016-04-21 18:35:54 +0200

    test

--- godisnji-odmori/src/main/java/tim12/si/app/godisnji_odmori/App.java
+++ godisnji-odmori/src/main/java/tim12/si/app/godisnji_odmori/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! TEST" );
+        System.out.println( "Hello World! TEST 1" );
     }
 }
