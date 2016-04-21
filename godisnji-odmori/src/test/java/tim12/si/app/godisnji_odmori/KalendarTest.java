259a91e047988aecd12320f186935b038872a7ec
kerimbalic <balickerim@gmail.com>
2016-04-21 19:14:16 +0200

    incijalni kod testova za kontrolere Odsustvo i Kalendar

---
+++ godisnji-odmori/src/test/java/tim12/si/app/godisnji_odmori/KalendarTest.java
@@ -0,0 +1,23 @@
+package tim12.si.app.godisnji_odmori;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.framework.Test;
+
+public class KalendarTest extends TestCase {
+	
+	public void testdajZauzeteDaneZaZaposlenika () {
+		
+		
+		assertTrue(true);
+		
+          }
+	
+	public void testprikaziKalendarZaZaposlenika () {
+		
+		
+		assertTrue(true);
+		
+          }
+
+}
---
+++ godisnji-odmori/src/test/java/tim12/si/app/godisnji_odmori/OdsustvoTest.java
@@ -0,0 +1,52 @@
+package tim12.si.app.godisnji_odmori;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class OdsustvoTest extends TestCase {
+	
+	
+	public void testdodajOdsustvo () {
+		
+	
+		assertTrue(true);
+		
+          }
+	
+	public void testdajOdsustvaPoTipu () {
+		
+		
+		assertTrue(true);
+		
+          }
+	
+	public void testdajOdsustvaPoZaposleniku () {
+		
+		
+		assertTrue(true);
+		
+          }
+	
+	public void testdajOdsustvaPoPeriodu () {
+		
+		
+		assertTrue(true);
+		
+          }
+	public void testdajOdsustvaPoSektoru () {
+		
+		
+		assertTrue(true);
+		
+          }
+	public void testdajSvaOdsustva () {
+		
+		
+		assertTrue(true);
+		
+          }
+
+	
+
+}
