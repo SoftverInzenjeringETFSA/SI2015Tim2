4e149b4e8eee476e428a709dd5a5f0d88030d197
Fahrudin123 <fbrbutovic1@etf.unsa.ba>
2016-04-22 18:09:41 +0200

    Testovi za izvjestaje i sektore
    Inicijalni kod za testove za kontrolere Sektor i Izvjestaj

---
+++ IzvjestajiTest.java
@@ -0,0 +1,28 @@
+package tim12.si.app.godisnji_odmori;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.framework.Test;
+
+public class IzvjestajiTest extends TestCase {
+	public void testdajGodisnjiIzvjestaj() {
+
+
+		assertTrue(true);
+
+          }
+
+	public void testdajMjesecniIzvjestaj() {
+
+
+		assertTrue(true);
+
+          }
+
+	public void testdajPreostaliBrojDanaPoKorisniku() {
+
+
+		assertTrue(true);
+
+          }
+}
