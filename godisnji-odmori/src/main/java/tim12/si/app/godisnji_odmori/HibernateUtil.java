34522611cdd3321ed1104fcec5e2fb01f5d6c7a4
tcerimagic <tcerimagic1@etf.unsa.ba>
2016-05-07 15:37:49 +0200

    BAZA RADI (Tile carobnjak)

--- godisnji-odmori/src/main/java/tim12/si/app/godisnji_odmori/HibernateUtil.java
+++ godisnji-odmori/src/main/java/tim12/si/app/godisnji_odmori/HibernateUtil.java
@@ -1,45 +1,25 @@
 package tim12.si.app.godisnji_odmori;
 
-
-
-import java.io.FileInputStream;
-import java.util.Properties;
-
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
 
-	private static final SessionFactory sessionFactory;
-	private static final ServiceRegistry serviceRegistry;
+	  private static final SessionFactory sessionFactory = buildSessionFactory();
 
-	static {
+	    private static SessionFactory buildSessionFactory() {
+	        try {
+	            // Create the SessionFactory from hibernate.cfg.xml
+	            return new Configuration().configure().buildSessionFactory();
+	        }
+	        catch (Throwable ex) {
+	            // Make sure you log the exception, as it might be swallowed
+	            System.err.println("Initial SessionFactory creation failed." + ex);
+	            throw new ExceptionInInitializerError(ex);
+	        }
+	    }
 
-		try {
-			java.util.Properties properties = new Properties();
-			properties.load(new FileInputStream("src/main/resources/db.properties"));
-		    Configuration configuration = new Configuration();
-		    configuration.configure().addProperties(properties);
-		    serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
-		            configuration.getProperties()).build();
-		    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-
-		} catch (Throwable ex) {
-
-			System.err.println("Initial SessionFactory creation failed." + ex);
-
-			throw new ExceptionInInitializerError(ex);
-
-		}
-
-	}
-
-	public static SessionFactory getSessionFactory() {
-
-		return sessionFactory;
-
-	}
-
+	    public static SessionFactory getSessionFactory() {
+	        return sessionFactory;
+	    }
 }
