57a1357283cc9bd909c59f4c07593c3fcb6d2687
Fahrudin123 <fbrbutovic1@etf.unsa.ba>
2016-05-11 00:04:10 +0200

    Izvjestaj modifikacije

--- godisnji-odmori/src/main/java/tim12/si/app/godisnji_odmori/Controller/IzvjestajController.java
+++ godisnji-odmori/src/main/java/tim12/si/app/godisnji_odmori/Controller/IzvjestajController.java
@@ -1,33 +1,49 @@
 package tim12.si.app.godisnji_odmori.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.hibernate.Session;
+
 import tim12.si.app.godisnji_odmori.ViewModel.*;
 import tim12.si.app.godisnji_odmori.Model.*;
+import tim12.si.app.godisnji_odmori.View.*;
 
 public class IzvjestajController {
 	
-	/**
-	 * 
+	private Session session;
+	
+	public IzvjestajController(Session session)
+	{
+		this.session=session;
+	}
+	
+	
+	/** 
 	 * @param godina
 	 */
 	public List<IzvjestajVM> dajGodisnjiIzvjestaj(int godina) {
-		// TODO - implement IzvjestajController.dajGodisnjiIzvjestaj
-		throw new UnsupportedOperationException();
+		IzvjestajVM ivm= new IzvjestajVM();
+		List<IzvjestajVM> lista = new ArrayList<IzvjestajVM>();
+		return lista;
 	}
 
 	/**
-	 * 
 	 * @param mjesec
 	 * @param godina
 	 */
 	public List<IzvjestajVM> dajMjesecniIzvjestaj(int mjesec, int godina) {
-		// TODO - implement IzvjestajController.dajMjesecniIzvjestaj
-		throw new UnsupportedOperationException();
+		IzvjestajVM ivm= new IzvjestajVM();
+		IzvjestajZapVM izvm= new IzvjestajZapVM();
+		List<IzvjestajVM> lista = new ArrayList<IzvjestajVM>();
+		return lista;
 	}
 
 	public List<IzvjestajZaposlenikBrDanaVM> dajPreostaliBrojDanaPoKorisniku() {
-		// TODO - implement IzvjestajController.dajPreostaliBrojDanaPoKorisniku
-		throw new UnsupportedOperationException();
+		IzvjestajVM ivm= new IzvjestajVM();
+		IzvjestajZaposlenikBrDanaVM izbrdvm= new IzvjestajZaposlenikBrDanaVM();
+		List<IzvjestajZaposlenikBrDanaVM> lista = new ArrayList<IzvjestajZaposlenikBrDanaVM>();
+		return lista;
 	}
 
 }
