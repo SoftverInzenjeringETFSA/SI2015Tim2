6e92807e54fa8a8ceb3b260a38dafa43e4416651
tcerimagic <tcerimagic1@etf.unsa.ba>
2016-05-15 23:36:17 +0200

    APPP

--- godisnji-odmori/src/main/java/tim12/si/app/godisnji_odmori/View/UI.java
+++ godisnji-odmori/src/main/java/tim12/si/app/godisnji_odmori/View/UI.java
@@ -14,6 +14,7 @@ import javax.swing.SwingUtilities;
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
 
+import tim12.si.app.godisnji_odmori.App;
 import tim12.si.app.godisnji_odmori.Singleton;
 import tim12.si.app.godisnji_odmori.Controller.PrisustvoController;
 import tim12.si.app.godisnji_odmori.Controller.ZaposlenikController;
@@ -57,6 +58,7 @@ public class UI {
 				try {
 					UI window = new UI();
 					window.frmLogin.setVisible(true);
+					App.main(null);
 				} catch (Exception e) {
 					
 					logger.error(e);
